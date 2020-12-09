1172b48d3812630d72a54efba8dbeb9a617d1ef2
Hello-Worker <75672831+Hello-Worker@users.noreply.github.com>
2020-12-09 22:49:09 +0900

    Add files via upload

---
+++ Python_Practice01.py
@@ -0,0 +1,23 @@
+a= 1+2j
+b= 3-4J
+print(a.real)
+print(a.imag)
+print(a.conjugate())
+print(abs(a))
+'''
+복소수.real 실수부분 리턴
+복소수.imag 허수부분 리턴
+복소수.conjugate() 켤레복소수 리턴
+'''
+
+a=4
+b=3
+print(a+b)
+print(a**b)
+print(a//b)
+
+'''
+사칙연산
+a**b a의b승
+// 나눗셈 후 소수점 아랫자리를 버리는 연산자
+'''
